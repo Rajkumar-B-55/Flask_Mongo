3c1dde76f14984190a7b83e54dd5c5a723bc60bc
Rajkumar <rajkumar.rk2403@gmail.com>
2023-07-19 21:27:49 +0530

    shell script

---
+++ startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+chmod +x startup.sh
+
+# Update pip in the virtual environment
+/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+
+pip install -r requirements.txt
+# Run your Flask application
+/opt/render/project/src/.venv/bin/python main_server.py
