0c57f45381e4ec5983fbce839e31576527754c0f
Rajkumar <rajkumar.rk2403@gmail.com>
2023-07-20 14:30:47 +0530

    environ changes

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ load_dotenv()
 
 
 class Config:
-    JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 
     # database credentials
-    DB_NAME = os.environ['DB_NAME']
-    DB_PASSWORD = os.environ['DB_PASSWORD']
-    DB_USERNAME = os.environ['DB_USERNAME']
-    DB_HOST = os.environ['DB_HOST']
-    DB_PORT = os.environ['DB_PORT']
-    DB_COLLECTION = os.environ['DB_COLLECTION']
+    DB_NAME = os.environ.get('DB_NAME')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD')
+    DB_USERNAME = os.environ.get('DB_USERNAME')
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_PORT = os.environ.get('DB_PORT')
+    DB_COLLECTION = os.environ.get('DB_COLLECTION')
